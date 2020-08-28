12f02d3cb919341a54867bd37ba8573027f9f779
Kim Jinseo <kiy2344@naver.com>
2020-08-28 18:58:10 +0900

    Main_Develop

---
+++ app/src/main/java/com/example/image_coversion/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.image_coversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ()
+    }
+}
